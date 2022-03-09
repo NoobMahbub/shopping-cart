058d53abba141dbc28573d0d9c13ef9e22053ab3
NoobMahbub <Mahbubbengali@gmail.com>
2022-03-09 13:49:46 +0600

    done

--- app.js
+++ app.js
@@ -1,1 +1,67 @@
-console.log('I am in a separate file')
+//updating case order number
+function updateProductNumber(product, price, isIncreasing) {
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = parseInt(productInput.value);
+
+    if (isIncreasing == true) {
+        productNumber = productNumber + 1;
+    }
+    else if (productNumber > 0) {
+        productNumber = productNumber - 1;
+
+    }
+    productInput.value = productNumber;
+
+    //updating total price
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productNumber * price;
+
+    calculateTotal();
+};
+
+function getInputValue(product) {
+
+    const productInput = document.getElementById(product + '-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+//
+
+function calculateTotal() {
+    
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const taxAmount = subTotal/10;
+    const totalPrice = subTotal + taxAmount;
+    //updating on html
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = taxAmount;
+    document.getElementById('total-price').innerText = totalPrice;
+    
+}
+
+
+//updating phone order number
+document.getElementById('phone-plus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, true);
+});
+document.getElementById('phone-minus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, false);
+});
+
+
+
+
+
+//updating case order number
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateProductNumber('case', 59, true);
+});
+
+document.getElementById('case-minus').addEventListener('click', function () {
+
+    updateProductNumber('case', 59, false);
+});
+
+
